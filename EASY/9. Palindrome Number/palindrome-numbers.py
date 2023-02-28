023778c036def635fee05fcbdd05d90448ab0acc
Savin Vladimir <savinvladimircs@gmail.com>
2023-02-28 09:41:36 +0300

    Delete all

--- EASY/9. Palindrome Number/palindrome-numbers.py
+++
@@ -1,7 +0,0 @@
-# Given an integer x, return true if x is a palindrome, and false otherwise.
-
-# Accepted
-
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
